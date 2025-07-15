83a6b0e5de1700289dba19ffc57247cb92120f12
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2025-07-15 19:16:03 +0900

    [Silver II] Title: 차이를 최대로, Time: 104 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -0,0 +1,35 @@
+
+diff_max = -1
+
+
+def sum_diff_list(arr):
+    temp = []
+    for i in range(0, len(arr) - 1):
+        diff_abs = abs(arr[i] - arr[i + 1])
+        temp.append(diff_abs)
+
+    return sum(temp)
+
+
+def permutation_visited(arr, result, visited, depth, r):
+    global diff_max
+
+    if depth == r:
+        check_max = sum_diff_list(result)
+        diff_max = diff_max if diff_max >= check_max else check_max
+        return
+
+    for i in range(len(arr)):  # 항상 모든원소를 순회
+        if not visited[i]:  # 원소가 선택되지 않았더라면
+            visited[i] = True  # 해당원소 사용
+            result[depth] = arr[i]  # 현재깊이에 원소사용
+            permutation_visited(arr, result, visited, depth + 1, r)  # 다음깊이 순회
+            visited[i] = False  # 해당원소 사용중지
+
+N = int(input())
+data = list(map(int, input().split()))
+result = [0] * len(data)
+visited = [False] * len(data)
+r = len(data)
+permutation_visited(data, result, visited, 0, r)
+print(diff_max)
