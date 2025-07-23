ab47f66ff03c05dfe249c2a0ccdc11128d07ae11
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2025-07-23 21:12:27 +0900

    [Silver IV] Title: 수 찾기, Time: 192 ms, Memory: 135764 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,31 +1,25 @@
 import sys
 
-
-def binary_search(arr, start, end, n):
-    if start > end:
-        return False
-
-    mid = (start + end) // 2  # start 0, end 11, mid 5
-
-    if n == arr[mid]:
-        return True
-    elif n > arr[mid]:
-        return binary_search(arr, mid + 1, end, n)
-    else:
-        return binary_search(arr, start, mid - 1, n)
-
-
-# 5
-# 4 1 5 2 3
-# 5
-# 1 3 7 9 5
-
 N = int(sys.stdin.readline())
-data = list(map(int, sys.stdin.readline().split()))
-data.sort()
+standard_list = list(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
-data2 = list(map(int, sys.stdin.readline().split()))
 
-for i in data2:
-    result = 1 if binary_search(data, 0, len(data) - 1, i) else 0
-    print(result)
+standard_list.sort()
+arr = list(map(int, sys.stdin.readline().split()))
+
+for i in range(M):
+    right = len(standard_list)
+    left = 0
+
+    while left < right:
+        mid = (right + left) // 2
+
+        if standard_list[mid] < arr[i]:
+            left = mid + 1
+        else:
+            right = mid
+
+    if left < N and standard_list[left] == arr[i]:
+        print(1)
+    else:
+        print(0)
