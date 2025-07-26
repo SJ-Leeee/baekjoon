204d3689a87333ccffa018a1c9b9cfe14e15b723
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2025-07-26 10:55:12 +0900

    [Gold IV] Title: 구슬 찾기, Time: 108 ms, Memory: 111540 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
@@ -1,5 +1,17 @@
-
 import sys
+
+
+def assemble_graph(graph, i):
+    visited = set()
+
+    def dfs(vertex):
+        visited.add(vertex)
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                dfs(neighbor)
+
+    dfs(i)
+    return len(visited) - 1
 
 
 def count_reachable(graph, start):
@@ -27,28 +39,22 @@ over_graph = {}
 under_graph = {}
 
 
-for i in range(1, V + 1):
+for i in range(1, V + 1):  # 정점 생성
     over_graph[i] = []
     under_graph[i] = []
 
 for _ in range(E):
     big, small = map(int, sys.stdin.readline().split())
     over_graph[small].append(big)
-    under_graph[big].append(small)
-
-
-# over_graph = assemble_graph(over_graph, V)
-
-
-# under_graph = assemble_graph(under_graph, V)
+    under_graph[big].append(small)  # 간선 생성
 
 
 count = 0
 limit = (V // 2) + 1
 
 for i in range(1, V + 1):
-    heavy_count = count_reachable(over_graph, i)
-    light_count = count_reachable(under_graph, i)
+    heavy_count = assemble_graph(over_graph, i)
+    light_count = assemble_graph(under_graph, i)
 
     if heavy_count >= limit or light_count >= limit:
         count += 1
