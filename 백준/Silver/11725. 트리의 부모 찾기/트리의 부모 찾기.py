4fa4b1809f20d3887cc68fe02796c0e99013e097
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2025-07-30 22:07:52 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 348 ms, Memory: 321648 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,65 +1,34 @@
-from collections import deque
 import sys
 sys.setrecursionlimit(100000)
 
-def dfs_parents(child_vertex):
+graph = {}
+V = int(sys.stdin.readline())
 
-    s_v = list(adjacency_list.keys())[0]
-    visited = set()
+for i in range(1, V + 1):
+    graph[i] = []
 
-    def dfs(vertex, parent):
-        if vertex == child_vertex:
-            return parent  # ← 찾았으면 즉시 반환
-
-        visited.add(vertex)
-
-        for neighbor in adjacency_list[vertex]:
-            if neighbor not in visited:
-                result = dfs(neighbor, vertex)
-                if result is not None:  # ← 결과가 있으면 즉시 반환
-                    return result
-
-        return None  # 못 찾았으면 None
-
-    pa = dfs(s_v, None)
-    return pa
+for _ in range(V - 1):
+    v1, v2 = map(int, sys.stdin.readline().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
 
 def dfs_recur():
-    s_v = list(adjacency_list.keys())[0]
-    result = []
-    visited = set()
+    previous = [False] * (V + 1)
+    visited = [False] * (V + 1)
 
-    def dfs(vertex, parents):
-        result.append([vertex, parents])
-        visited.add(vertex)
+    def dfs(v):
+        visited[v] = True
 
-        for item in adjacency_list[vertex]:
-            if item not in visited:
-                dfs(item, vertex)
+        for n_v in graph[v]:
+            if not visited[n_v]:
+                previous[n_v] = v
+                dfs(n_v)
 
-    dfs(s_v, None)
-    return result
+    dfs(1)
+    return previous
 
 
-adjacency_list = {}
-
-N = int(sys.stdin.readline())
-
-for i in range(1, N + 1):
-    adjacency_list[i] = []
-
-for _ in range(N - 1):
-    v1, v2 = map(int, sys.stdin.readline().split())
-    adjacency_list[v1].append(v2)
-    adjacency_list[v2].append(v1)
-
-# for i in range(2, N + 1):
-#     i_parents = dfs_parents(i)
-#     print(i_parents)
-
-re = dfs_recur()
-re.sort(key=lambda x: x[0])
-
-for i in range(1, len(re)):
-    print(re[i][1])
+a = dfs_recur()
+for i in range(2, V + 1):
+    print(a[i])
