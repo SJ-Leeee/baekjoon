4ae8528a75c5f9101d26dc4cda4d69a7eeea8594
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2025-07-30 20:59:43 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,61 +1,32 @@
-
-from collections import deque
 import sys
 
 
-def dfs_recur():
-    s_v = list(adjancy_list.keys())[0]
-    result = []
-    visited = set()
+graph = {}
+V = int(sys.stdin.readline())
+E = int(sys.stdin.readline())
 
-    def dfs(vertex):
-        result.append(vertex)
-        visited.add(vertex)
+for i in range(1, V + 1):
+    graph[i] = []
 
-        for item in adjancy_list[vertex]:
-            if item not in visited:
-                dfs(item)
-
-    dfs(s_v)
-    return result
-
-
-def bfs(s_v):
-    if s_v == None:
-        s_v = list(adjancy_list.keys())[0]
-
-    dq = deque()
-    result = []
-    visited = set()
-    dq.append(s_v)
-    visited.add(s_v)
-
-    while len(dq) > 0:
-        removed = dq.popleft()
-
-        result.append(removed)
-        for item in adjancy_list[removed]:
-            if item not in visited:
-                visited.add(item)
-                dq.append(item)
-
-    return result
-
-
-adjancy_list = {}
-
-N = int(sys.stdin.readline())
-
-M = int(sys.stdin.readline())
-for i in range(1, N + 1):
-
-    adjancy_list[i] = []
-
-for _ in range(M):
+for _ in range(E):
     v1, v2 = map(int, sys.stdin.readline().split())
-    adjancy_list[v1].append(v2)
-    adjancy_list[v2].append(v1)
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
-re1 = dfs_recur()
 
-print(len(re1) - 1)
+def dfs_recursion():
+    result = []
+    visited = [False] * (V + 1)
+
+    def dfs(v):
+        visited[v] = True
+        result.append(v)
+        for n_v in graph[v]:
+            if not visited[n_v]:
+                dfs(n_v)
+
+    dfs(1)
+    print(len(result) - 1)
+
+
+dfs_recursion()
