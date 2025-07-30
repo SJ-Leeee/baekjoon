46ef9794bcd70a47f8693b5a782cb20d242119bd
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2025-07-30 20:30:28 +0900

    [Silver II] Title: DFS와 BFS, Time: 136 ms, Memory: 114848 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,140 +1,61 @@
+
 from collections import deque
-import heapq
 import sys
 
 
-class Graph:
-    def __init__(self):
-        self.adjacency_list = {}
+def dfs_iterative(graph, start):
+    """반복적 DFS - 스택 오버플로우 방지"""
+    visited = [False] * (V + 1)
+    result = []
+    stack = [start]
 
-    def add_vertex(self, vertex):
-        if vertex not in self.adjacency_list:
-            self.adjacency_list[vertex] = []
-            return True
-        return False
+    while stack:
+        node = stack.pop()
+        if not visited[node]:
+            visited[node] = True
+            result.append(node)
+            # 역순으로 추가해야 올바른 순서 보장
+            for neighbor in reversed(graph[node]):
+                if not visited[neighbor]:
+                    stack.append(neighbor)
 
-    def add_edge(self, vertex1, vertex2):  # 무방향? 노방향!
-        if not (vertex1 in self.adjacency_list and vertex2 in self.adjacency_list):
-            return False
-        self.adjacency_list[vertex1].append(vertex2)
-        self.adjacency_list[vertex2].append(vertex1)
-        return True
-
-    def remove_vertex(self, vertex):
-        if not vertex in self.adjacency_list:
-            return False
-        while len(self.adjacency_list[vertex]) > 0:
-            removed, cost = self.adjacency_list[vertex].pop()
-            # item[0]이 정점 이름
-            self.adjacency_list[removed] = [
-                item for item in self.adjacency_list[removed] if item[0] != vertex
-            ]
-
-        del self.adjacency_list[vertex]
-
-    def remove_edge(self, vertex1, vertex2):
-        if not (vertex1 in self.adjacency_list and vertex2 in self.adjacency_list):
-            return False
-
-        self.adjacency_list[vertex1] = [
-            item for item in self.adjacency_list[vertex1] if item[0] != vertex2
-        ]
-        self.adjacency_list[vertex2] = [
-            item for item in self.adjacency_list[vertex2] if item[0] != vertex1
-        ]
-        return True
-
-    def dfs_search(self, ver):
-        if ver not in self.adjacency_list:
-            return []
-        result = []
-        visited = set()
-
-        def dfs(ver):
-
-            result.append(ver)
-            visited.add(ver)
-
-            self.adjacency_list[ver].sort(key=lambda x: x)
-
-            for v in self.adjacency_list[ver]:
-                if v not in visited:
-                    dfs(v)
-
-        dfs(ver)
-
-        return result
-
-    def bfs_search(self, ver):
-        if ver not in self.adjacency_list:
-            return []
-
-        dq = deque()
-        dq.append(ver)
-        result = []
-        visited = set()
-        visited.add(ver)
-
-        while len(dq) > 0:
-            ver = dq.popleft()
-            result.append(ver)
-
-            self.adjacency_list[ver].sort(key=lambda x: x)
-            for v in self.adjacency_list[ver]:
-                if v not in visited:
-                    visited.add(v)
-                    dq.append(v)
-        return result
-
-    def mst_prim(self, start):
-        if not start in self.adjacency_list:
-            return []
-        """
-        일단은 전부다 힙으로 만들어
-        """
-        mst = []
-        visited = set()
-
-        heap = [[0, start, start]]
-        while heap and len(mst) < len(self.adjacency_list) - 1:
-            cost, from_v, to_v = heapq.heappop(heap)
-
-            if to_v in visited:
-                continue
-
-            visited.add(to_v)
-
-            if from_v != to_v:
-                mst.append([cost, from_v, to_v])
-
-            for v, c in self.adjacency_list[to_v]:
-                if v not in visited:
-                    heapq.heappush(heap, [c, to_v, v])
-
-        return mst
+    print(*result)
 
 
-"""
-4 5 1 정점의 개수, 간선의 개수, 시작할정점의 번호
-1 2
-1 3
-1 4
-2 4
-3 4
-"""
+def bfs(graph, root):
+    visited = [False] * (V + 1)
+    result = []
+    dq = deque()
+    dq.append(root)
 
-v, e, to_v = map(int, sys.stdin.readline().split())
+    while dq:
+        vertex = dq.popleft()
 
-g = Graph()
+        if visited[vertex]:
+            continue
+        visited[vertex] = True
+        result.append(vertex)
+        for neighbor in graph[vertex]:
+            if not visited[neighbor]:
+                dq.append(neighbor)
 
-for i in range(1, v + 1):
-    g.add_vertex(i)
-for _ in range(e):
+    print(*result)
+
+
+graph = {}
+V, E, S = map(int, sys.stdin.readline().split())
+
+for i in range(1, V + 1):
+    graph[i] = []
+
+for _ in range(E):
     v1, v2 = map(int, sys.stdin.readline().split())
-    g.add_edge(v1, v2)
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
-dfs_result = g.dfs_search(to_v)
-bfs_result = g.bfs_search(to_v)
 
-print(*dfs_result)
-print(*bfs_result)
+for i in range(1, V + 1):
+    graph[i].sort()
+
+dfs_iterative(graph, S)
+bfs(graph, S)
