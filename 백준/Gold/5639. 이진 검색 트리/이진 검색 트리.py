97d530baa237f5557ed6e06a70b8d856d01d929f
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2025-07-30 17:09:08 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 496 ms, Memory: 128516 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,3 +1,6 @@
+import sys
+
+sys.setrecursionlimit(10000)
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -9,40 +12,41 @@ class Tree:
     def __init__(self):
         self.root = None
 
-    def set_node(self, node):
-        if self.root == None:
+    def insert_node(self, node):
+        if not self.root:
             self.root = node
         else:
             prev_node = None
             cur_node = self.root
-            while cur_node != None:
-                if cur_node.val < node.val:
-                    prev_node = cur_node
-                    cur_node = cur_node.right
-                else:
+
+            while cur_node:
+                if cur_node.val > node.val:
                     prev_node = cur_node
                     cur_node = cur_node.left
-            if prev_node.val < node.val:
-                prev_node.right = node
-            else:
+                else:
+                    prev_node = cur_node
+                    cur_node = cur_node.right
+
+            if prev_node.val > node.val:
                 prev_node.left = node
+            else:
+                prev_node.right = node
 
-    def post_order(self, node):
-        if node.left:
-            self.post_order(node.left)
-        if node.right:
-            self.post_order(node.right)
+    def post_order(self):
+        result = []
 
-        print(node.val)
-        return
+        def post_dfs(node):
+            if not node:
+                return
 
-    def get_root(self):
-        return self.root
+            post_dfs(node.left)
+            post_dfs(node.right)
+            result.append(node.val)
+
+        post_dfs(self.root)
+        return result
 
 
-import sys
-
-sys.setrecursionlimit(10000)
 tree = Tree()
 
 while True:
@@ -53,7 +57,8 @@ while True:
     # 처리 로직
     value = int(value)
     node = TreeNode(value)
-    tree.set_node(node)
+    tree.insert_node(node)
 
-root_node = tree.get_root()
-tree.post_order(root_node)
+a = tree.post_order()
+for i in a:
+    print(i)
