d7f0d170f8a478e49ebc6e881bd558b8bc3c9f76
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2025-07-31 00:49:21 +0900

    [Gold III] Title: 아침 산책, Time: 852 ms, Memory: 552844 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,43 +1,60 @@
-
-def dfs_recursive(start):
-    visited = set()
-    cnt = 0
-
-    def dfs(vertex):
-        nonlocal cnt  # 외부 cnt 수정
-        visited.add(vertex)
-        for neighbor in adjacency_list[vertex]:  # list{ 1: [2,3]}
-
-            if neighbor not in visited:
-                if is_inside[neighbor - 1] == 1:
-                    cnt += 1
-                else:
-                    dfs(neighbor)
-
-    dfs(start)
-    return cnt
-
-
 import sys
 
-adjacency_list = {}
+sys.setrecursionlimit(200000)
 
+graph = {}
 N = int(sys.stdin.readline())
-
-is_inside = list(map(int, sys.stdin.readline().strip()))
-
+is_inside = [0] + list(map(int, sys.stdin.readline().strip()))
 for i in range(1, N + 1):
-    adjacency_list[i] = []
+    graph[i] = []
 
 for _ in range(N - 1):
     v1, v2 = map(int, sys.stdin.readline().split())
-    adjacency_list[v1].append(v2)
-    adjacency_list[v2].append(v1)
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
 
-count = 0
-for i in range(1, len(is_inside) + 1):
-    if is_inside[i - 1] == 1:
-        count += dfs_recursive(i)
+def morning_walk(graph, is_inside):
+    visited = set()
+    acc_cnt = 0
+    temp_cnt = 0
 
-print(count)
+    def dfs_out(start):
+        nonlocal temp_cnt
+        visited.add(start)
+
+        for n_v in graph[start]:
+            if is_inside[n_v]:  # 실내라면 숫자만 추가하고
+                temp_cnt += 1
+            else:  # 실외면 더 들어가자
+                if n_v not in visited:
+                    dfs_out(n_v)
+
+    def dfs_in(start):
+        nonlocal temp_cnt
+        visited.add(start)
+
+        for n_v in graph[start]:
+            if is_inside[n_v] and n_v not in visited:  # 방문하지 않았고, 실내면 추가
+                temp_cnt += 1
+                dfs_in(n_v)
+            # else: # 방문한적이 있거나, 실외면
+            # temp_cnt = 0
+
+    for i in range(1, len(is_inside)):
+        if not is_inside[i] and i not in visited:  # 0이고 방문하지 않았던 실외
+            dfs_out(i)
+            acc_cnt += temp_cnt * (temp_cnt - 1)
+            temp_cnt = 0
+        if is_inside[i] and i not in visited:
+
+            dfs_in(i)
+            temp = temp_cnt if temp_cnt > 0 else 0
+            acc_cnt += temp * 2
+            temp_cnt = 0
+
+    print(acc_cnt)
+    # dfs
+
+
+morning_walk(graph, is_inside)
