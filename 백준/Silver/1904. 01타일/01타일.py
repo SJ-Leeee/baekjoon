3c0fd7faeb44bb7aa177e16a164f9ea5dddb649b
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2025-08-06 20:48:29 +0900

    [Silver III] Title: 01타일, Time: 100 ms, Memory: 117360 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,20 +1,17 @@
 import sys
-
-
-def zero_one_tile(n):
-    if n < 3:
-        return n
-    dp = [0 for _ in range(n + 1)]
-    dp[1] = 1
-    dp[2] = 2
-
-    for i in range(3, n + 1):
-        dp[i] = (dp[i - 1] % 15746) + (dp[i - 2] % 15746)
-
-    return dp[n] % 15746
-
-
 N = int(sys.stdin.readline())
 
-result = zero_one_tile(N)
-print(result)
+def zero_one (N):
+    if N < 3: 
+        print(N)
+        return
+    dp = [0] * (N+1)
+    dp[1] = 1
+    dp[2] = 2
+    
+    for i in range(3, N+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 15746
+        
+    print(dp[N])    
+    
+zero_one(N)
