2a98162c5625bef4977e0f54e09c175fc61fefdf
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2025-08-06 21:39:19 +0900

    [Gold V] Title: 동전, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,23 +1,20 @@
 import sys
 
 
-def coin_count(num, coins):
+S = int(sys.stdin.readline())
+
+for _ in range(S):
+    C = int(sys.stdin.readline())
+    coins = list(map(int, sys.stdin.readline().split()))
+    num = int(sys.stdin.readline())
+
     dp = [0] * (num + 1)
     dp[0] = 1
-    for coin in coins:  # 1,2
 
+    for coin in coins:
         for i in range(num + 1):
             if i >= coin:
                 dp[i] += dp[i - coin]
 
-    return dp[-1]
+    print(dp[-1])
 
-
-N = int(sys.stdin.readline())
-
-for _ in range(N):
-    M = int(sys.stdin.readline())
-    coins = list(map(int, sys.stdin.readline().split()))
-    num = int(sys.stdin.readline())
-    a = coin_count(num, coins)
-    print(a)
