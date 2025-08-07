bbd585a546c11cba299a95d6673726706003db34
Lee SeungJun <132549231+SJ-Leeee@users.noreply.github.com>
2025-08-07 11:24:42 +0900

    [Silver V] Title: 거스름돈, Time: 96 ms, Memory: 111112 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,20 @@
+import sys
+
+
+N = int(sys.stdin.readline())
+
+coins = [2, 5]
+
+coin_dp = [float("inf")] * (N + 1)
+
+coin_dp[0] = 0
+
+
+for i in range(2, N + 1):
+    if i < 5:
+        coin_dp[i] = coin_dp[i - 2] + 1
+    else:
+        coin_dp[i] = min(coin_dp[i - 2] + 1, coin_dp[i - 5] + 1)
+
+result = coin_dp[-1] if coin_dp[-1] != float("inf") else -1
+print(result)
